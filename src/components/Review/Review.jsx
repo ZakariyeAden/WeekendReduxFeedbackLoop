698a7c45895ba979408847355d445c582d40cd94
ZakariyeAden <zakariye5417@gmail.com>
2023-07-15 19:19:50 -0500

    styling a little and displaying it in the review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
-import { Button } from '@mui/material'
+import React from "react";
+import { Button } from "@mui/material";
 // HOOKS
-import { useSelector } from 'react-redux'
-
+import { useSelector } from "react-redux";
 
 const Review = () => {
-
+  // Display the information from the store.Use useSelector
+  const feeling = useSelector(state => state.feeling);
+  const support = useSelector(state => state.support);
+  const comment = useSelector(state => state.comment);
+  const understanding = useSelector(state => state.understanding);
   return (
-    <div>
-   
+    <div className="review">
+      <h2>Review Your Feedback</h2>
+      <ul>
+        <li>Feelings:{feeling}</li>
+        <li>Understanding:{understanding}</li>
+        <li>Support:{support}</li>
+        <li>Comments:{comment}</li>
+      </ul>
+      <Button variant="contained">Submit</Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
     </div>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
