15df30029088a71714f81063fff5bb841484230f
ZakariyeAden <zakariye5417@gmail.com>
2023-07-15 19:08:13 -0500

    update

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import { Button } from '@mui/material'
 // HOOKS
 import { useSelector } from 'react-redux'
-// Components
-import ReviewItem from '../ReviewItem/ReviewItem'
+
+
 const Review = () => {
-  // const feedbacks = useSelector(state => state.feedback);
+
   return (
     <div>
-     {/* {feedbacks.map((review) => {
-       return(
-         <ReviewItem reveiw={review} key={review.id}/>
-       );
-     })}    */}
+   
     </div>
   )
 }
